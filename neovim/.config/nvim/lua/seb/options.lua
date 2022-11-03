d32c421085c015cc91f5b64e9c3187516cd24993
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2022-11-03 09:57:40 +0100

    feat: add neovim (WIP)

---
+++ neovim/.config/nvim/lua/seb/options.lua
@@ -0,0 +1,8 @@
+-- Common neovim options
+vim.opt.number = true			-- display line number
+vim.opt.relativenumber = true		-- enable relative numbers
+vim.opt.ignorecase = true		-- ignore case sensitivity ...
+vim.opt.smartcase = true		-- unless we typed an upper case 
+vim.opt.tabstop = 2			-- number of spaces for Tab character
+vim.opt.shiftwidth = 2			-- amount of characters for indentation
+vim.opt.expandtab = true		-- Tab is expanded as spaces
