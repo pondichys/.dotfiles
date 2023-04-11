344b2b840028e9ea9e2117ca0fb847e4135d7812
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-04-11 23:04:10 +0200

    feat(neovim): add more icons

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -32,7 +32,12 @@ return {
   -- icons for plugins
   {
     "nvim-tree/nvim-web-devicons",
-    config = true,
+    dependencies = { "DaikyXendo/nvim-material-icon" },
+    config = function()
+      require("nvim-web-devicons").setup {
+        override = require("nvim-material-icon").get_icons(),
+      }
+    end,
   },
   -- indentation guides
   {
