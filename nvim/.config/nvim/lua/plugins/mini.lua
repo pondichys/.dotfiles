135f319a8b094804df7cd8106081f890d5c7121c
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-04-15 10:02:24 +0200

    feat(neovim): add bunch of mini plugins

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,55 @@
+-- Some mini plugins
+return {
+  -- animate to have some nice animations
+  {
+    "echasnovski/mini.animate",
+    version = "*",
+    config = function(_, _)
+      require("mini.animate").setup()
+    end,
+  },
+  -- comments
+  {
+    "echasnovski/mini.comment",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
+  -- auto complete pairs
+  {
+    "echasnovski/mini.pairs",
+    version = "*",
+    config = function()
+      require("mini.pairs").setup({})
+    end,
+  },
+  -- easy manipulate surround character
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      -- use gz mappings instead of s to prevent conflict with leap
+      require("mini.surround").setup(opts)
+    end,
+  },
+}
