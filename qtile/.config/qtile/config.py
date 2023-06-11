3a20d6314449fe0883825ad7df07007177e4bc25
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-06-11 07:42:09 +0200

    feat(qtile): add FR keyboard support

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -81,8 +81,11 @@ groups = []
 # FOR QWERTY KEYBOARDS
 # group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
 
-# FOR AZERTY KEYBOARDS
-group_names = ["ampersand", "eacute", "quotedbl", "apostrophe", "parenleft", "section", "egrave", "exclam", "ccedilla", "agrave",]
+# # FOR AZERTY BE KEYBOARDS
+# group_names = ["ampersand", "eacute", "quotedbl", "apostrophe", "parenleft", "section", "egrave", "exclam", "ccedilla", "agrave",]
+
+# FOR AZERTY FR KEYBOARDS
+group_names = ["ampersand", "eacute", "quotedbl", "apostrophe", "parenleft", "minus", "egrave", "underscore", "ccedilla", "agrave",]
 
 group_labels = ["1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "0",]
 # group_labels = ["", "", "", "", "", "", "", "", "", "",]
